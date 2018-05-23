9f35308eaeb9825ce76ace928e97f1d165ad6d6d
dspdamith <damithkum565@gmail.com>
2018-05-23 09:14:20 +0530

    Successfully added

---
+++ developmentbackend/src/main/java/dam/kum/developmentbackend/App.java
@@ -0,0 +1,13 @@
+package dam.kum.developmentbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
